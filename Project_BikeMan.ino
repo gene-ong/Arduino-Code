ed9d9fbdc2fd4e945c08cb765d37dac3f500a61d
Unknown <gene.cl.ong@gmail.com>
2018-08-13 23:53:07 +1000

    Working with interrupts now
    Using pin 7 as an interrupt value2 is incremented when pin 7's state changes

--- Project_BikeMan.ino
+++ Project_BikeMan.ino
@@ -1,58 +1,64 @@
 #include <Adafruit_NeoPixel.h>
-#define NbPix 9
+#include <EnableInterrupt.h>
+#define ARDUINOPIN 7
+
+
+
+#define NbPix 15
 #define LedPin 12
-#define SwitchPin 7
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NbPix, LedPin, NEO_GRB + NEO_KHZ800);
-
-bool SwitchState = false;
 
 // We need to declare the data exchange
 // variable to be volatile - the value is
 // read from memory.
 volatile int value = 0;
 volatile int value2 = 0;
-int filter = 10;
+int filter = 4;
+
+void interruptFunction() {
+  value2++;
+}
+
+
 void setup() {
+  Serial.begin(9600);
+  enableInterrupt(ARDUINOPIN, interruptFunction, CHANGE);
+
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  colorWipe(strip.Color(255, 0, 0), 10); // Red
-  // sets the digital pin as input
-  pinMode(SwitchPin, INPUT);
 }
 
 void loop() {
-  if (digitalRead(SwitchPin) == true)
+
+  Serial.print("Pin was interrupted: ");
+  Serial.print(value2);
+  Serial.println(" times so far.");
+
+  for (uint16_t i = 0; i < value; i++)
   {
-    SwitchState = false;
-
-    for (uint16_t i = 0; i <= value; i++)
-    {
-      strip.setPixelColor(i, strip.Color(0, 255, 0));
-      strip.show();
-      delay(1);
-    }
-    value2++;
-    if (value2 == filter)
-    {
-      value++;
-      if (value > NbPix)
-      {
-        value = 0;
-        colorWipe(strip.Color(255, 0, 0), 10); // Red
-      }
-      value2 = 0;
-    }
-
-
+    strip.setPixelColor(i, strip.Color(0,255, 0));
+    strip.show();
+    delay(1);
   }
 
+  if (value2 >= filter)
+  {
+    value++;
+    value2 = 0;
+  }
+  if (value == 0)
+    colorWipe(strip.Color(255, 0, 0), 0); // Red
+  if (value > NbPix)
+  {
+    value = 0;
+  }
 
 }
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
-  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+  for (uint16_t i = 0; i < NbPix; i++) {
     strip.setPixelColor(i, c);
     strip.show();
     delay(wait);
